840e40b9c9d47a034e5fc9296a274045aa24f652
Marco Garduño <46688349+Themindscupltor@users.noreply.github.com>
2019-04-01 13:31:52 -0600

    Update Running_Ipyrad_GBS.sh

--- IPyRad/Running_Ipyrad_GBS.sh
+++ IPyRad/Running_Ipyrad_GBS.sh
@@ -5,13 +5,13 @@ $ mkdir proyecto_radseq
 $ cd ./proyecto_radseq
 
 # We have to download all the data inside the 'Abies GBS DATA' folder in our new working directoy. After that, we gonna make a parameter file using ipyrad
-./ipyrad -n prueba3 
+$ ./ipyrad -n prueba3 
 
 # You can use the 'param-prueba3.txt' parameter file inside the `data` folder in order to put the same parameters used.
 # the data in this exercise it's already demultiplexes. However, Ipyrad workflow demand to execute all the 7 steps. So
 # this is how you can make it.
 
-./ipyrad -p params-prueba3.txt -s 1234567
+$ ./ipyrad -p params-prueba3.txt -s 1234567
 
 # On my Macbook Air, Ipyrad takes arround 12 hours to run. So, go outside, work in something else.
 
